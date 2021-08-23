b2c307bb8cbd745201f8883fd2e080fb9fef176c
b0xmaker <68164843+b0xmaker@users.noreply.github.com>
2021-08-23 12:03:31 -0400

    Updated file with comments.

--- temperature_LED.ino
+++ temperature_LED.ino
@@ -1,7 +1,11 @@
-// Thermistor Example #3 from the Adafruit Learning System guide on Thermistors
+// Simple temperature indicator based on TM1637 LED display and thermistor.
+// Original thermistor code from: Thermistor Example #3 from the Adafruit Learning System guide on Thermistors
 // https://learn.adafruit.com/thermistor/overview by Limor Fried, Adafruit
 // Industries MIT License - please keep attribution and consider buying parts
 // from Adafruit
+// connect 10K NTC thermistor between A0 and ground, connect 10K resistor between 3V3 and A0.
+// connect TM637 to power/5V, connect CLK to 20 and DIO to 21.
+// TM1637 library found in library manager or https://github.com/avishorp/TM1637
 
 // which analog pin to connect
 #define THERMISTORPIN A0
@@ -20,21 +24,26 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
 // Module connection pins (Digital Pins)
-#define CLK 21
-#define DIO 20
+#define CLK 20
+#define DIO 21
 
 TM1637Display display(CLK, DIO);
 
 int samples[NUMSAMPLES];
 
 void setup(void) {
-  Serial.begin(9600);
+  // initialize serial monitor and analog reference pin for thermistor
+  Serial.begin(115200);
   analogReference(EXTERNAL);
+
+  // initialize LED display with max brightness, clear display 
   display.setBrightness(0x0f);
   display.clear();
 }
 
 void loop(void) {
+
+  // start counter and average values
   uint8_t i;
   float average;
 
@@ -55,6 +64,7 @@ void loop(void) {
   average = 1023 / average - 1;
   average = SERIESRESISTOR / average;
 
+  // convert resistance to temperature in celcius 
   float steinhart;
   steinhart = average / THERMISTORNOMINAL;          // (R/Ro)
   steinhart = log(steinhart);                       // ln(R/Ro)
@@ -63,9 +73,10 @@ void loop(void) {
   steinhart = 1.0 / steinhart;                      // Invert
   steinhart -= 273.15; // convert absolute temp to C
 
+  // print temperature in serial monitor, show temperature (decimal form) on display
   Serial.print("Ambient temperature:");
   Serial.print(steinhart);
-  Serial.println(" *C");
+  Serial.println(" C");
   display.showNumberDec(steinhart);
 
   delay(1000);
